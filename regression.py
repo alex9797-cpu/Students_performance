89addfbf5034e33ceb850c026ecafccaad2368db
Alexander Brandmayr <brandi99@live.at>
2022-08-07 20:13:08 +0200

    fitted Linear Model and added Notebook for analysis

--- regression.py
+++ regression.py
@@ -3,8 +3,13 @@
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
-from utils import concat_dummy
+from utils import concat_dummy , metrics_table
 from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import StandardScaler
+from sklearn.linear_model import LinearRegression
+from sklearn.model_selection import cross_val_score
+from sklearn.tree import DecisionTreeRegressor
+from sklearn.metrics import r2_score, mean_squared_error , mean_absolute_error, max_error
 
 # Import Data
 students_data=pd.read_csv(filepath_or_buffer='student-mat.csv',sep=';')
@@ -21,7 +26,7 @@ students_data.drop(columns=['G1','G2'], inplace=True)
 
 # Change all categrocial variables to dummy variables:
 
-cat_features=['school','sex','address','famsize','Pstatus','Mjob', 'Fjob', 'reason', 'guardian','schoolsup', 'famsup', 'paid', 'activities', 'nursery','higher', 'internet']
+cat_features=['school','sex','address','famsize','Pstatus','Mjob', 'Fjob', 'reason', 'guardian','schoolsup', 'famsup', 'paid', 'activities', 'nursery','romantic','higher', 'internet']
 
 
 for variable in cat_features:
@@ -38,6 +43,78 @@ X_train,X_test, y_train,y_test= train_test_split(X,y,test_size=0.25,random_state
 
 print(f' Number of training  instances {X_train.shape[0]}')
 print(f' Number of test  instances {X_test.shape[0]}')
+
+# Perform feature scaling on train and test set:
+
+numeric_features=['age','Medu','Fedu','traveltime','studytime','failures','famrel','freetime','goout','Dalc','Walc','health','absences']
+
+scaler=StandardScaler()
+
+X_train[numeric_features]=scaler.fit(X_train[numeric_features]).transform(X_train[numeric_features])
+X_test[numeric_features]=scaler.fit(X_test[numeric_features]).transform(X_test[numeric_features])
+
+# Fit a Linear Regression model:
+linear_model=LinearRegression()
+# Perform 10 fold cross validation
+
+scores=cross_val_score(linear_model, X_train, y_train, cv=5,scoring='neg_mean_squared_error')
+print(f' Cross Validation results Linear Model{scores}')
+print('\n')
+
+# Fit A Regression Tree_:
+
+tree_model=DecisionTreeRegressor(random_state=0)
+
+scores=cross_val_score(tree_model, X_train, y_train, cv=5,scoring='neg_mean_squared_error')
+print(f' Cross Validation results Regression Tree {scores}')
+print('\n')
+
+
+
+# Check Performance on Testset:
+linear_model.fit(X_train,y_train)
+y_pred1=linear_model.predict(X_test)
+
+
+lm_results=  metrics_table(y_test,y_pred1)
+
+print(lm_results)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
--- utils.py
+++ utils.py
@@ -1,5 +1,6 @@
 ## Contains used fucntion
 import pandas as pd
+from sklearn.metrics import r2_score, mean_squared_error , mean_absolute_error, max_error
 
 
 # Function for creating dummy variable and adding it to the dataframe
@@ -11,6 +12,22 @@ def concat_dummy(df:pd.DataFrame,colname:str):
     df=df.drop(columns=colname)
 
     return(df)
+
+
+def metrics_table(y_true,y_pred):
+    MSE=mean_squared_error(y_true,y_pred)
+    MAE=mean_absolute_error(y_true,y_pred)
+    Max_error=max_error(y_true,y_pred)
+    r2=r2_score(y_true,y_pred)
+
+    d={'Metric': ['MSE','MAE','Max_error','R2'],'Value': [MSE,MAE,Max_error,r2]}
+
+    return pd.DataFrame(data=d)
+
+
+
+
+
 
 
 
